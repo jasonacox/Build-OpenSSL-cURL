3a417bb992ee79de8690e36f7ecd136d7d0567d6
Jason Cox <jason.cox@disney.com>
2016-03-02 20:41:09 -0800

    Set LC_CTYPE=C to help with sed errors on some systems

--- openssl/openssl-build.sh
+++ openssl/openssl-build.sh
@@ -119,6 +119,7 @@ buildTVOS()
 	export CROSS_SDK="${PLATFORM}${TVOS_SDK_VERSION}.sdk"
 	export BUILD_TOOLS="${DEVELOPER}"
 	export CC="${BUILD_TOOLS}/usr/bin/gcc -fembed-bitcode -arch ${ARCH}"
+	export LC_CTYPE=C
    
 	echo "Building ${OPENSSL_VERSION} for ${PLATFORM} ${TVOS_SDK_VERSION} ${ARCH}"
 
