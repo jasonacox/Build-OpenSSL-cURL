a4c4180bafaa6655ed41c1952ae713904fa8b095
Jason Cox <jason.cox@disney.com>
2016-04-16 17:51:43 -0700

    Fix clean.sh to remove tvOS

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Cleaning Build-OpenSSL-cURL"
-rm -fr curl/curl-* curl/include curl/lib openssl/openssl-1* openssl/Mac openssl/iOS openssl/tvO
+rm -fr curl/curl-* curl/include curl/lib openssl/openssl-1* openssl/Mac openssl/iOS openssl/tvOS
