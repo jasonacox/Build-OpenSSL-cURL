bbf6a49782218f4818842ab6a023a4f12891825d
Jason A. Cox <jason@jasonacox.com>
2021-09-15 22:17:56 -0700

    Updated versions

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@
 # EDIT this section to Select Default Versions #
 ################################################
 
-OPENSSL="1.1.1i"	# https://www.openssl.org/source/
-LIBCURL="7.74.0"	# https://curl.haxx.se/download.html
-NGHTTP2="1.42.0"	# https://nghttp2.org/
+OPENSSL="1.1.1l"	# https://www.openssl.org/source/
+LIBCURL="7.79.0"	# https://curl.haxx.se/download.html
+NGHTTP2="1.44.0"	# https://nghttp2.org/
 
 ################################################
 
