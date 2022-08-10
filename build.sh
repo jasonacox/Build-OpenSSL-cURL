6cb186e92cb7eed6006200229502c35932e03693
Jason A. Cox <jason@jasonacox.com>
2022-08-09 20:08:57 -0700

    OpenSSL 1.1.1q fails to build - roll back

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@
 #   https://github.com/jasonacox/Build-OpenSSL-cURL
 #
 
+set -e
+
 ################################################
 # EDIT this section to Select Default Versions #
 ################################################
 
-OPENSSL="1.1.1q"	# https://www.openssl.org/source/
+OPENSSL="1.1.1p"	# https://www.openssl.org/source/
 LIBCURL="7.84.0"	# https://curl.haxx.se/download.html
 NGHTTP2="1.48.0"	# https://nghttp2.org/
 
